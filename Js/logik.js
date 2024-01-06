1ad998b45083bf3d0cb04dfbcb1e350f45de6cec
DBaku <dimaba487@gmail.com>
2024-01-06 21:56:35 +0100

    try add addtocart

---
+++ Js/logik.js
@@ -0,0 +1,27 @@
+let cart = [1];
+function addToCart(pizza) {
+    cart.push(pizza);
+    updateTotalPrice();
+}
+function updateTotalPrice() {
+    let total = 0;
+
+    switch (cart[cart.length - 1]) {
+        case "pizza-salame":
+            total += 9;
+            break;
+
+        case "pizza-funghi":
+            total += 9;
+            break;
+
+        case "pizza-margherita":
+            total += 7;
+            break;
+        case "pizza-spinaci":
+            total += 9;
+            break;
+    }
+
+    document.getElementById("total-price").innerHTML = "Gesamtpreis: " + total.toFixed(2) + "€";
+}
